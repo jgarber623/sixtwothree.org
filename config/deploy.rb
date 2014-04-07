c43d354cab76218c46815dce32eeb15b7b2dbe87
Jason Garber <jason@sixtwothree.org>
2014-04-06 22:23:55 -0400

    Cleanup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 set :stages, %w(production)
 set :default_stage, 'production'
+
 require 'capistrano/ext/multistage'
 
 set :application, 'sixtwothree.org'
@@ -8,7 +9,6 @@ set :keep_releases, 3
 set :scm, :git
 set :repository, 'git@github.com:jgarber623/sixtwothree.org.git'
 set :ssh_options, {:forward_agent => true}
-
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
