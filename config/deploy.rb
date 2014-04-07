18188b95f0d7793199a51d705379b28d117a6359
Jason Garber <jason@sixtwothree.org>
2014-04-06 22:03:18 -0400

    Add/override deploy tasks.

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,3 +11,19 @@ set :ssh_options, {:forward_agent => true}
 
 set :deploy_via, :remote_cache
 set :use_sudo, false
+
+after :deploy, 'deploy:after_deploy', 'deploy:cleanup'
+
+namespace :deploy do
+  task :after_deploy do
+    # Build site to public folder
+  end
+
+  task :finalize_update do
+    # Override default deploy:finalize_update task
+  end
+
+  task :restart do
+    # Override default deploy:restart task
+  end
+end
