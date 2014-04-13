7cec0a9948243e91b1e5bf7db3073340aefa1ca2
Jason Garber <jason@sixtwothree.org>
2014-04-12 21:57:28 -0400

    Add all the right pieces to get the deploy working.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,22 +5,22 @@ require 'capistrano/ext/multistage'
 
 set :application, 'sixtwothree.org'
 
-set :keep_releases, 3
-set :scm, :git
 set :repository, 'git@github.com:jgarber623/sixtwothree.org.git'
-set :ssh_options, {:forward_agent => true}
 set :deploy_via, :remote_cache
+set :ssh_options, {:forward_agent => true}
 set :use_sudo, false
 
-after :deploy, 'deploy:after_deploy', 'deploy:cleanup'
+set :ruby_version, '2.1.1'
+set :bundle_cmd, "chruby-exec #{ruby_version} -- bundle"
+
+default_run_options[:shell] = '/bin/bash'
+
+after :deploy, 'deploy:cleanup'
 
 namespace :deploy do
-  task :after_deploy do
-    run "cd #{current_path} && bundle && bundle exec jekyll build --config config/jekyll.yml"
-  end
-
   task :finalize_update do
     # Override default deploy:finalize_update task
+    run "cd #{release_path} && #{bundle_cmd} && #{bundle_cmd} exec jekyll build --config config/jekyll.yml"
   end
 
   task :restart do
