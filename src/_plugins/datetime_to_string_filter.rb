010217c4d2b6b0f11acd53ce15937ac1c1f432b1
Jason Garber <jason@sixtwothree.org>
2014-07-04 16:24:06 -0400

    Update ordinalize method.

--- src/_plugins/datetime_to_string_filter.rb
+++ src/_plugins/datetime_to_string_filter.rb
@@ -1,7 +1,16 @@
 module Jekyll
   module DatetimeToStringFilter
     def datetime_to_string(input)
-      input.strftime("%B %e, %Y at %l:%M %P %Z")
+      input.strftime("%B #{ordinalize input.day}, %Y at %l:%M %P %Z")
+    end
+
+    def ordinalize(input)
+      case input
+        when 1, 21, 31; "#{input}<sup>st</sup>"
+        when 2, 22;     "#{input}<sup>nd</sup>"
+        when 3, 23;     "#{input}<sup>rd</sup>"
+        else            "#{input}<sup>th</sup>"
+      end
     end
   end
 end
