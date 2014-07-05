46593399eb1d072e853e03fb43c3f3e06b07a736
Jason Garber <jason@sixtwothree.org>
2014-07-05 00:45:52 -0400

    Stay dry. #12

--- src/_plugins/datetime_to_string_filter.rb
+++ src/_plugins/datetime_to_string_filter.rb
@@ -5,12 +5,14 @@ module Jekyll
     end
 
     def ordinalize(input)
-      case input
-        when 1, 21, 31; "#{input}<sup>st</sup>"
-        when 2, 22;     "#{input}<sup>nd</sup>"
-        when 3, 23;     "#{input}<sup>rd</sup>"
-        else            "#{input}<sup>th</sup>"
+      ordinal = case input
+        when 1, 21, 31; 'st'
+        when 2, 22;     'nd'
+        when 3, 23;     'rd'
+        else            'th'
       end
+
+      "#{input}<sup>#{ordinal}</sup>"
     end
   end
 end
