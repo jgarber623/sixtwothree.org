1761a916d320c3a19660748e3d5ade9f68f4fd77
Jason Garber <jason@sixtwothree.org>
2014-07-16 17:18:47 -0400

    Shorten up application.js.

--- src/_assets/javascripts/application.js
+++ src/_assets/javascripts/application.js
@@ -1,18 +1,14 @@
 require(['vendor/routerrouter'], function(RouterRouter) {
-	var router = new RouterRouter({
-		routes: {
-			'blog/:slug': 'post'
-		},
+	var router = new RouterRouter();
 
-		post: function() {
-			require(['modules/mentions'], function(Mentions) {
-				new Mentions({
-					endpoint: 'http://webmention.sixtwothree.org/api/webmentions',
-					params: {
-						'target': location.href
-					}
-				});
+	router.route('blog/:slug', function() {
+		require(['modules/mentions'], function(Mentions) {
+			new Mentions({
+				endpoint: 'http://webmention.sixtwothree.org/api/webmentions',
+				params: {
+					'target': location.href
+				}
 			});
-		}
+		});
 	});
 });
