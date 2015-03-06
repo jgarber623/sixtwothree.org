a02f53eb2c3b976570b80d37850e425ae5200ef2
Jason Garber <jason@sixtwothree.org>
2015-03-06 16:45:44 -0500

    Shorten up dem vars.

--- app/assets/javascripts/admin/syndication-form.js
+++ app/assets/javascripts/admin/syndication-form.js
@@ -3,21 +3,21 @@
 }(this, function() {
 	var SyndicationForm = function($el) {
 		this.$el = $el;
-		this.$nameField = $el.querySelector('input[type="text"]');
-		this.$urlField = $el.querySelector('input[type="url"]');
+		this.$name = $el.querySelector('input[type="text"]');
+		this.$url = $el.querySelector('input[type="url"]');
 	};
 
 	SyndicationForm.prototype.init = function() {
-		this.$urlField.addEventListener('keyup', this.keyup.bind(this));
+		this.$url.addEventListener('keyup', this.keyup.bind(this));
 	};
 
 	SyndicationForm.prototype.keyup = function(event) {
-		var val = this.$urlField.value;
+		var val = this.$url.value;
 
 		if (val.match(/^https?:\/\//)) {
 			var hostname = val.match(/^https?:\/\/(?:[a-zA-Z]+\.){0,1}([a-zA-Z0-9][a-zA-Z0-9-]+){1}\.[A-Za-z]{2,}/)[1];
 
-			this.$nameField.value = hostname.charAt(0).toUpperCase() + hostname.slice(1);
+			this.$name.value = hostname.charAt(0).toUpperCase() + hostname.slice(1);
 		}
 	};
 
