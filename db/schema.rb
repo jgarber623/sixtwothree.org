f49c3272f0f266b4d7bf6d63dbcfd1c7d2d69930
Jason Garber <jason@sixtwothree.org>
2015-03-08 16:24:38 -0400

    Update acts-as-taggable-on gem.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150107153747) do
+ActiveRecord::Schema.define(version: 20150308201818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
