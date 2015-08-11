e9e581012b10b3dba56efec54c79465c8673adc0
Jason Garber <jason@sixtwothree.org>
2015-08-10 20:12:39 -0400

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150510235959) do
+ActiveRecord::Schema.define(version: 20150810233720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20150510235959) do
     t.datetime "verified_at"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.text     "fragmention"
   end
 
   create_table "taggings", force: :cascade do |t|
