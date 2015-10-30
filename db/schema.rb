38807ec9419a1e59994bd37ad2921a173d44f184
Jason Garber <jason@sixtwothree.org>
2015-10-30 16:17:15 -0400

    Updates with new CarrierWave-related gems, udpated schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150810233720) do
+ActiveRecord::Schema.define(version: 20151030160826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20150810233720) do
     t.text     "author_photo_url"
     t.text     "author_url"
     t.datetime "published_at"
+    t.text     "author_avatar"
   end
 
   create_table "francis_cms_webmentions", force: :cascade do |t|
