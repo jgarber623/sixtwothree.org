8849e68dee9a37e1481e9574ba52e4bb1c3a2fd9
Jason Garber <jason@sixtwothree.org>
2015-11-03 22:12:15 -0500

    Updates after adding svgeez. Remove assets rake task.

--- app/assets/javascripts/modules/entry.js
+++ app/assets/javascripts/modules/entry.js
@@ -8,7 +8,7 @@
 			var $shim = document.createElement('i'),
 				$title = this.$el.querySelector('.entry-title');
 
-			$shim.innerHTML = '<svg><use xlink:href="#icon-' + this.$el.getAttribute('data-entry-type') + '"></svg>';
+			$shim.innerHTML = '<svg><use xlink:href="#icons-' + this.$el.getAttribute('data-entry-type') + '"></svg>';
 
 			$title.parentNode.insertBefore($shim, $title);
 		}
