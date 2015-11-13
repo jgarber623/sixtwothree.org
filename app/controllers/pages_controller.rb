605ac7704a79a02dc9390314d2db56462ce531ec
Jason Garber <jason@sixtwothree.org>
2015-11-13 00:26:12 -0500

    Add recent photos to homepage.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def homepage
-    @posts = FrancisCms::Post.recently_published
     @links = FrancisCms::Link.recently_published
+    @photos = FrancisCms::Photo.recently_published
+    @posts = FrancisCms::Post.recently_published
   end
 end
