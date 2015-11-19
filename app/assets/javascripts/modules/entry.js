51f6d09af8deeda1ad23d3792cf7fe4c08963fbf
Jason Garber <jason@sixtwothree.org>
2015-11-19 12:17:04 -0500

    Add version attribute to SVG elements.

--- app/assets/javascripts/modules/entry.js
+++ app/assets/javascripts/modules/entry.js
@@ -10,7 +10,7 @@
 			if ($title) {
 				var $shim = document.createElement('i');
 
-				$shim.innerHTML = '<svg><use xlink:href="#icons-' + this.$el.getAttribute('data-entry-type') + '"></svg>';
+				$shim.innerHTML = '<svg role="img" version="1.1"><use xlink:href="#icons-' + this.$el.getAttribute('data-entry-type') + '"></svg>';
 				$title.parentNode.insertBefore($shim, $title);
 			}
 		}
