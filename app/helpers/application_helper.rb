e2bbea01e359893ee73e7a23f02d660e691f8ce4
Jason Garber <jason@sixtwothree.org>
2015-11-28 22:17:45 -0500

    Step up that metadata game (see: https://adactio.com/journal/9881)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,14 @@
 module ApplicationHelper
+  def page_card_type
+    @photo ? 'summary_large_image' : 'summary'
+  end
+
   def page_description
     @page_description || francis_cms_config.site_description
+  end
+
+  def page_image
+    @photo ? @photo.photo.url(:medium) : francis_cms_config.user_avatar
   end
 
   def page_title
