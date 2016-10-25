32b1d6ea8ea5bfc7dbd0e7e2cae76ebf446e5ac3
Jason Garber <jason@sixtwothree.org>
2016-10-24 21:52:43 -0400

    Add flash notices to sessions_spec.

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'sessions', type: :request do
 
       expect(response).to have_http_status(:redirect)
       expect(response).to redirect_to('/')
+      expect(flash[:notice]).to eq('You’ve successfully logged out. See you again soon!')
     end
   end
 
@@ -34,24 +35,30 @@ RSpec.describe 'sessions', type: :request do
 
       expect(response).to have_http_status(:redirect)
       expect(response).to redirect_to('/')
+      expect(flash[:notice]).to eq('Welcome back! You look nice today.')
     end
   end
 
   describe 'GET #new' do
+    context 'when logged in' do
+      before do
+        allow_any_instance_of(ApplicationController).to receive(:logged_in?).and_return(true)
+      end
+
+      it 'redirects to the homepage.' do
+        get '/login'
+
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to('/')
+        expect(flash[:notice]).to eq('You’re already logged in, boss!')
+      end
+    end
+
     it 'renders the login page.' do
       get '/login'
 
       expect(response).to have_http_status(:success)
       expect(response).to render_template('sessions/new')
-    end
-
-    it 'redirects to the homepage.' do
-      allow_any_instance_of(ApplicationController).to receive(:logged_in?).and_return(true)
-
-      get '/login'
-
-      expect(response).to have_http_status(:redirect)
-      expect(response).to redirect_to('/')
     end
   end
 end
