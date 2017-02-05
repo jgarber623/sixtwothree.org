ed5c9781dfebaa1309ca0f2f34da2aa4cc3eaae9
Jason Garber <jason@sixtwothree.org>
2017-02-04 19:28:43 -0500

    Reorganize request specs.

--- spec/requests/pages/homepage_spec.rb
+++ spec/requests/pages/homepage_spec.rb
@@ -1,22 +1,4 @@
 RSpec.describe PagesController, type: :request do
-  describe 'GET #colophon' do
-    before do
-      get '/colophon'
-    end
-
-    it 'returns HTTP success status code' do
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'renders the colophon template' do
-      assert_template('colophon')
-    end
-
-    it 'displays the page heading' do
-      assert_select 'h1', text: 'Colophon'
-    end
-  end
-
   describe 'GET #homepage' do
     before do
       get '/'
