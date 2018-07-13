5ed222314e60be393b36a8a150f334a7b55c0734
Jason Garber <jason@sixtwothree.org>
2018-07-12 23:56:57 -0400

    Update linked directories

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :deploy_to, '/var/www/sixtwothree-org'
 append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, 'log', 'public/uploads', 'tmp/cache', 'tmp/pids', 'tmp/sockets'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
