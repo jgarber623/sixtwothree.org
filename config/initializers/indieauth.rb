ee949e51d0a1d71d8102343e2b0fc6d13643dc6c
Jason Garber <jason@sixtwothree.org>
2019-01-22 20:28:10 -0500

    Add microsub endpoint to markup

--- config/initializers/indieauth.rb
+++ config/initializers/indieauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.tap do |config|
   config.authorization_endpoint = 'https://indieauth.com/auth'
+  config.microsub_endpoint      = 'https://aperture.p3k.io/microsub/219'
   config.token_endpoint         = 'https://tokens.indieauth.com/token'
 end
