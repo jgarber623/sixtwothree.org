e8e2c8ec0e2355fbf36470403cc1ec50090bea14
Jason Garber <jason@sixtwothree.org>
2019-08-07 11:47:26 -0400

    Use authenticate.computer

--- config/initializers/indieauth.rb
+++ config/initializers/indieauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.tap do |config|
-  config.authorization_endpoint = 'https://indieauth.com/auth'
+  config.authorization_endpoint = 'https://authenticate.computer/auth'
   config.microsub_endpoint      = 'https://aperture.p3k.io/microsub/219'
-  config.token_endpoint         = 'https://tokens.indieauth.com/token'
+  config.token_endpoint         = 'https://authenticate.computer/token'
 end
