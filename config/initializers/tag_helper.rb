e8e6055cbcb1d84fab4bbf8937be407109bb3c4b
Jason Garber <jason@sixtwothree.org>
2021-01-18 11:14:39 -0500

    Disable Style/OptionalBooleanParameter cop

--- config/initializers/tag_helper.rb
+++ config/initializers/tag_helper.rb
@@ -1,11 +1,11 @@
 module ActionView
   module Helpers
     module TagHelper
-      # rubocop:disable Rails/OutputSafety
+      # rubocop:disable Rails/OutputSafety, Style/OptionalBooleanParameter
       def tag(name, options = nil, open = true, escape = true)
         "<#{name}#{tag_options(options, escape) if options}#{open ? '>' : ' />'}".html_safe
       end
-      # rubocop:enable Rails/OutputSafety
+      # rubocop:enable Rails/OutputSafety, Style/OptionalBooleanParameter
 
       def boolean_tag_option(key)
         key
